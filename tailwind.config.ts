79e3086faa43242eb415b644c93a7f9921a7d518
박승희 <ridia05031@gmail.com>
2025-08-04 13:34:35 +0900

    feat: 상품 관리 페이지 구현

---
+++ tailwind.config.ts
@@ -0,0 +1,14 @@
+const config = {
+    content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+    theme: {
+        extend: {
+            fontFamily: {
+                sans: ['Pretendard Variable', 'sans-serif'],
+                pretendard: ['Pretendard', 'sans-serif'],
+            },
+        },
+    },
+    plugins: [],
+};
+
+export default config;
